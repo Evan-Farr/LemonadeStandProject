bc29bd9a6c9ee16e1b3fdfaecc2e00d8edb3cd67
Evan-Farr <evan.c.farr@gmail.com>
2017-04-10 17:07:11 -0500

    finished test 6

--- LemonadeStand/LemonadeStandTestProject/UnitTest1.cs
+++ LemonadeStand/LemonadeStandTestProject/UnitTest1.cs
@@ -35,5 +35,70 @@ namespace LemonadeStandTestProject
             //Assert
             Assert.AreEqual(inventory.Lemons, result);
         }
+
+        [TestMethod]
+        public void Weather_DetermineDemand()
+        {
+            //Arrange
+            Random random = new Random();
+            Weather weather = new Weather(random);
+            string skyCover = "overcast";
+            string temperature = "cold";
+            string result = "low";
+            //Act
+            weather.DetermineDemand(skyCover, temperature);
+            //Assert
+            Assert.AreEqual(weather.LemonadeDemand, result);
+        }
+
+        [TestMethod]
+        public void Store_CalculateRunningProfitLoss()
+        {
+            //Arrange
+            Random random = new Random();
+            Player player = new Player("hank", random);
+            Store store = new Store(random, player);
+            double dailySales = 100;
+            store.Money = 600;
+            double result = 200; 
+            //Act
+            store.CalculateRunningProfitLoss(dailySales);
+            //Assert
+            Assert.AreEqual(store.RunningProfitLoss, result);
+        }
+
+        [TestMethod]
+        public void Store_UpdateMoney()
+        {
+            //Arrange
+            Random random = new Random();
+            Player player = new Player("hank", random);
+            Store store = new Store(random, player);
+            double sales = 100;
+            store.Money = 600;
+            double result = 700;
+            //Act
+            store.UpdateMoney(sales);
+            //Assert
+            Assert.AreEqual(store.Money, result);
+        }
+
+        [TestMethod]
+        public void Store_CalculateDailyProfitLoss()
+        {
+            //Arrange
+            Random random = new Random();
+            Player player = new Player("hank", random);
+            Store store = new Store(random, player);
+            double dailySales = 300;
+            store.DailyExpenses = 100;
+            double result = 200;
+            //Act
+            store.CalculateDailyProfitLoss(dailySales);
+            //Assert
+            Assert.AreEqual(store.DailyProfitLoss, result);
+        }
+
+
     }
 }
